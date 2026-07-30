9cf4e307ef54f4160d692cde4c7a4af39b3ef629
Willie Shen <cg10250207@gmail.com>
2026-07-30 20:07:59 +0800

    l

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-rm -rf ~/.vim_runtime
+rm -rf ~/.vim_runtime || true
 git clone --depth=1 https://github.com/Willie169/vim-config.git ~/.vim_runtime
 cd ~/.vim_runtime/pack/my-plugins/start
 git clone --depth=1 https://github.com/lilydjwg/fcitx.vim.git
