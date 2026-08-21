671977a5508aeb12a67595f5d7a814690c330294
Willie Shen <cg10250207@gmail.com>
2026-08-21 18:38:31 +0800

    2026-08-21T10:38:31+00:00

--- basic.vim
+++ basic.vim
@@ -415,3 +415,10 @@ if ! has('nvim')
 	set undodir=~/.vim_runtime/undodir
 	set undofile
 endif
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => fcitx.vim
+"    https://github.com/lilydjwg/fcitx.vim
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+let g:fcitx5_rime = 1
--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 rm -rf ~/.vim_runtime || true
 git clone --depth=1 https://github.com/Willie169/vim-config.git ~/.vim_runtime
 cd ~/.vim_runtime/pack/my-plugins/start
+git clonee --depth=1 https://github.com/lilydjwg/fcitx.vim.git
 git clone --depth=1 https://github.com/Willie169/termux-clipboard.git
 git clone --depth=1 https://github.com/jasonccox/vim-wayland-clipboard.git
 ln -sf "${HOME}"/.vim_runtime/vimrc "${HOME}"/.vimrc
